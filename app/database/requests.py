d5b639ed3532136ccfb6f071977e949cb20dc9a5
uly <ulyana.akinfina22@gmail.com>
2024-05-08 14:52:50 +0300

    type hinting add

--- app/database/requests.py
+++ app/database/requests.py
@@ -3,7 +3,7 @@ from app.database.models import User, Dict
 from sqlalchemy import select
 
 
-async def set_user(tg_id):
+async def set_user(tg_id: int):
     async with async_session() as session:
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
 
@@ -12,7 +12,7 @@ async def set_user(tg_id):
             await session.commit()
 
 
-async def save_to_database(user_question, bot_answer, user):
+async def save_to_database(user_question: str, bot_answer: str, user: int):
     async with async_session() as session:
         translation = Dict(fraze=user_question, answer=bot_answer, user=user)
         session.add(translation)
